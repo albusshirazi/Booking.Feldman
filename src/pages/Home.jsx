8f0a69461c88bacb94b2740123a2ee5237f17ca1
ShiraShirazi <albus.shirazi@gmail.com>
2021-09-22 20:19:56 +0300

    change banner title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,10 +10,10 @@ export default function Home() {
         <>
         <Hero hero="defaultHero">
         </Hero>
-        <Banner title="Luxurious Rooms" subtitle="deluxe rooms starting at 300$">
-                <Link to="/rooms" className="btn btn-primary">
+        <Banner title="ברוכים הבאים" subtitle="גלול מטה כדי לשריין מתקנים ברחבי הבסיס">
+                {/* <Link to="/rooms" className="btn btn-primary">
                       Our Rooms
-                </Link>
+                </Link> */}
         </Banner>
         <Services/> 
         <FeaturedRooms/>
